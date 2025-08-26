d46eb51dce89c7468bbd07a1105bc38c42694b33
User <User@DESKTOP-B115L06>
2025-08-26 14:15:36 +1000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NumberToWords.Models;
+
+namespace NumberToWords.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Convert number to words
+        [HttpPost]
+        public IActionResult Convert(decimal number)
+        {
+            var words = NumberToWords((long)number);
+            var cents = (int)((number - Math.Floor(number)) * 100);
+
+            ViewBag.Words = $"{words} Dollar{(number == 1 ? "" : "s")}{(cents > 0 ? $" and {NumberToWords(cents)} Cent{(cents == 1 ? "" : "s")}" : "")}";
+            return View("Index");
+        }
+
+        private string NumberToWords(long number)
+        {
+            if (number == 0)
+                return "Zero";
+
+            if (number < 0)
+                return "Minus " + NumberToWords(Math.Abs(number));
+
+            string[] unitsMap = new string[]
+            {
+                "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
+                "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+            };
+
+            string[] tensMap = new string[]
+            {
+                "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
+                "Seventy", "Eighty", "Ninety"
+            };
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " Million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " Thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " Hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words.Trim();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NumberToWords.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
